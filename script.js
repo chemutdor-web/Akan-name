e1aaa99dc56d22fbd33e6f02ea47ec58b686541e
chemuu <chemutdor@gmail.com>
2026-04-28 12:49:29 -0700

    Fix script source reference in index.html and update variable declarations in script.js; enhance button styling in style.css

--- script.js
+++ script.js
@@ -2,40 +2,49 @@
 //  main event
 document.getElementById('Akan-form').addEventListener('submit',function(event){
     event.preventDefault();
+let birthday = document.getElementById("birthday").value;
+let genderInput = document.querySelector('input[name="gender"]:checked');
+    // validation
+    if(!birthday || !gender){
+        alert("please select both your birthday and gender!");
+        return;
 
-const maleNames = ["Kwasi", "Kwado", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
- const femaleNames = ["akosua", "Adwoa", "Abena", "Akua", "yaa", "Afua", "Ama"];
+
+let  maleNames = ["Kwasi", "Kwado", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+ let femaleNames = ["akosua", "Adwoa", "Abena", "Akua", "yaa", "Afua", "Ama"];
 
     // input values
-    const birthday = document.getElementById('birthday').value;
-    const gender = document.querySelector('input[name="gender"]:checked').value;
-    const display = document.getElementById('displayname');
+    // let birthday = document.getElementById('birthday').value;
+    let genderInput = document.querySelector('input[name="gender"]:checked').value;
+    let display = document.getElementById('displayname');
 
-    // validation
+    // validation makes sure they actually picked the gender
     if(!birthday || !gender){
         alert("please select both your birthday and gender!");
         return;
     }
     // get input values
-     let gender = genderInput.value;
+      let gender = genderInput.value;
      let date = new Date(birthday);
 
     //  extract date parts
       let MM = date.getMonth() + 1;
       let DD = date.getDate(); 
       let Year = date.getFullYear();
+      
 
     //   define century(cc) and year of the cenury (YY)
-    let CC = Math.floor(year /100);
+    let CC = Math.floor(year / 100);
     let YY = year % 100;
 
     let d = (((CC/4) -2 * CC - 1) + Math.floor(5 * YY / 4) + Math.floor(26 * (MM + 1) / 10) +DD) % 7;
 
     // get the index 
     // we use math.floor and the ensure the result is positive
+    // fix negative result
     let index = Math.floor(d);
     if (index < 0){
-        index = index + 7;
+        index += 7;
     }
     // match name and display
     let akanName="";
@@ -47,6 +56,7 @@ const maleNames = ["Kwasi", "Kwado", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"]
     }
 
     // Display result 
-    document.getElementById('display').innerHTML = akanName;
+    document.getElementById("displayname").innerHTML =
+      "Your Akan Name is <span>" +akanName;"</span>"
 
 });
