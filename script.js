e99d17820fb7ba91ed6cf6a104b05fc697f42e51
chemuu <chemutdor@gmail.com>
2026-04-28 02:44:17 -0700

    Fix form structure and enhance validation in script.js; update display name handling in index.html

--- script.js
+++ script.js
@@ -9,8 +9,38 @@ document.getElementById('akanForm').addEventListener('submit',function(event){
     const gender = document.querySelector('input[name="gender"]:checked').value;
     const display = document.getElementById('displayname');
 
-
+    // validation
+    if(!birthday || !gender){
+        alert("please select both your birthday and gender!");
+        return;
+    }
     // process the date
+    const dateParts = birthday.split("-");
+    const yearStr = dateParts[0];
+    const MM = parseInt(dateParts[1]);
+    const DD = parseInt(dateParts[2]);
+
+    let d = (((CCC/4) -2*CC-1) +((5*YY/4)) +((26*(MM+1)/10)) +DD) %7;
+    let index = Math.floor(d);
+    if (index < 0){
+        index = index + 7;
+    }
+    // match name and display
+    let akanName="";
+    if (gender=== "male"){
+        akanName= malesNames[index]
+
+    } else {
+        akanName= femaleNames[index];
+
+    }
+    display.innerHTML = your Akan name is <str
+
+
+
+
+
+
 
 
 
