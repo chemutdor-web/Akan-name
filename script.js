cdb759792597408586ad03c2eb49f454b44ac698
chemuu <chemutdor@gmail.com>
2026-04-29 08:57:34 +0300

    Fix gender input handling to retrieve checked value and correct calculation logic in script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 document.getElementById('Akan-form').addEventListener('submit',function(event){
     event.preventDefault();
 const birthday = document.getElementById("birthday").value;
-const gender = document.querySelector('input[name="gender"]:checked').value;
+const gender = document.querySelector('input[name="gender"]:checked')
     // validation
     if(!birthday || !gender){
         alert("please select both your birthday and gender!");
@@ -27,7 +27,7 @@ const gender = document.querySelector('input[name="gender"]:checked').value;
     // this the calculation
 
     let d =
-      (CC / 4 -
+      Math.floor(CC / 4 -
         2 * CC -
         1 +
         Math.floor((5 * YY) / 4) +
